dbbbbc90e6578d37e0d6cbf2e7eb65ccaa789252
Kyle Brown <kyle_brown@intuit.com>
2017-03-21 12:38:43 -0700

    refactor

--- index.js
+++ index.js
@@ -1,101 +1,197 @@
 'use strict';
 const request = require('request-promise');
 const memoizee = require('memoizee');
-const IFTT_KEY = process.env.IFTT_KEY;
-const IFTT_EVENT = process.env.IFTT_EVENT;
-const STOP_NAME = process.env.STOP_NAME;
-class ACE {
+const ACTION = process.argv[2];
+
+class Train {
+    
     constructor(options) {
-        this.baseUrl = 'https://www.acerail.com/CMSWebParts/ACERail/TrainStatusService.aspx';
-        this.serviceCache = memoizee(this._getService, {
+        this.id = options.equipmentID;
+        this.geo = {
+            lat: options.lat,
+            lng: options.lng
+        };
+        this.delayedMinutes = options.onSchedule;
+        this.scheduleNumber = options.scheduleNumber;
+        this.inService = !!options.inService;
+        this.stops = options.stops;
+        this.nextStopID = options.nextStopID;
+        this.minutesToNextStops = options.minutesToNextStops;
+
+    }
+    _getStopById(id) {
+        return this.stops.find((stop) => stop.id === id);
+    }
+    _getMinutesToNextStopById(id) {
+        return this.minutesToNextStops.find((stop) => stop.stopID.toString() === id.toString());
+    }
+    get time() {
+        return `${(this.delayedMinutes < 0) ? Math.abs(this.delayedMinutes) + ' min late' : 'On Time'}`;
+    }
+    get nextStop() {
+        let next = this._getStopById(this.nextStopID);
+        let time = this._getMinutesToNextStopById(this.nextStopID);
+        return `Next stop: ${next ? next.name : 'unknown'}. ETA: ${time ? time.status : 'unknown'}`;
+    }
+    get status() {
+        return `Train ${this.scheduleNumber} is ${this.time}. ${this.nextStop}.`;
+    }
+    
+}
+
+class Stop {
+    constructor(options) {
+        this.routeID = options.rid;
+        this.id = options.id;
+        this.name = options.name;
+        this.geo = {
+            lat: options.lat,
+            lng: options.lng
+        };
+        this.shortName = options.shortName;
+        // minutsToNextStops?
+    }
+}
+
+/**
+ * Altimont Corridor Express Status
+ * 
+ * @class ACE
+ */
+class ACE {
+    /**
+     * Creates an instance of ACE.
+     * @param {object} options Options object 
+     * 
+     * @memberOf ACE
+     */
+    constructor(options) {
+        this.ifttKey = options.ifttKey,
+        this.ifttEvent = options.ifttEvent;
+        this._serviceCache = memoizee(this._getService, {
             promise: true,
             maxAge: 3000
         });
+        this.useSeed = options.useSeed || false;
+        this.seed = options.seed || {};
     }
-    stops() {
-        return this.getService('get_stops');
+    /**
+     * Retrieves the baseURL of the API service.
+     * 
+     * @readonly
+     * @returns {string} API Url
+     * @memberOf ACE
+     */
+    get _baseUrl() {
+        return 'https://www.acerail.com/CMSWebParts/ACERail/TrainStatusService.aspx';
     }
-
-    vehicles() {
-        return this.getService('get_vehicles');
-    }
-    getService(service) {
-        return this.serviceCache(service);
-    }
+    /**
+     * Query the ACE service.
+     * 
+     * @param {string} service The sub-service to query ['get_vehicles', 'get_stops']
+     * @returns Promise
+     * 
+     * @memberOf ACE
+     */
     _getService(service) {
-        return request({
-            uri: `${this.baseUrl}?service=${service}`,
-            json: true
-        }).then((data) => {
-            return data[service];
-        });
-    }
-    
-    getTrainsTime (stopName) {
-        return this.getTrains().then((trains) => {
-            return trains.map((train) => {
-                let stop = train.minutesToNextStops.find((stopTime) => stopTime.name === stopName);
-                if (stop) {
-                     return {
-                        inService: train.inService,
-                        stop: stop,
-                        onSchedule: train.onSchedule,
-                        scheduledTime: stop.schedule,
-                        train: train.scheduleNumber,
-                        status: `[${train.scheduleNumber} to ${stopName}. ETA: ${(stop.status === 'On Time' ? stop.schedule : stop.status)}. Late: ${train.onSchedule} mins.]`
-                    };
-                    
-                } else {
-                    return {};
-                }
-               
-            }).filter((item) => Object.keys(item).length);
-        });
-    }
+        if (this.useSeed) {
+            return new Promise((resolve) => {
+                resolve(this.seed[service]);
+            });
+        } else {
+            return request({
+                uri: `${this._baseUrl}?service=${service}`,
+                json: true
+            }).then((data) => {
+                return data[service];
+            });
+        }
 
-    getTrains() {
-        return this.vehicles().then((vehicles) => {
-            return this.stops().then((stops) => {
-                stops = stops.reduce((map, stop) => {
-                    map[stop.id] = stop;
-                    return map;
-                }, {});
-                return vehicles.map((vehicle) => {
-                    vehicle.minutesToNextStops = vehicle.minutesToNextStops.map((stop) => {
-                        stop.name = stops[stop.stopID].name;
-                        return stop;
-                    });
-                    return vehicle;
+    }
+    /**
+     * Retrieves a list of Trains.
+     * 
+     * @param {Boolean} inServiceOnly Only get in service trains
+     * @returns Promise.<Train[]>
+     * 
+     * @memberOf ACE
+     */
+    getTrains(inServiceOnly) {
+        return this.getStops().then((stops) => {
+            return this._serviceCache('get_vehicles').then((trains) => {
+                if (inServiceOnly) {
+                    trains = trains.filter((train) => train.inService);
+                }
+                return trains.map((train) => {
+                    train.stops = stops;
+                    return new Train(train);
                 });
             });
         });
+        
     }
-    sendStatus (status) {
-        console.log('Sending status: ', status);
+    getStops() {
+        return this._serviceCache('get_stops').then((stops) => {
+            return stops.map((stop) => new Stop(stop));
+        });
+    }
+    _postIFTT(data) {
+        if (!this.ifttEvent && !this.ifttKey) {
+            throw new Error('IFTT Event & Key not provided');
+        }
+        console.log(data);
         return request({
-            uri: `https://maker.ifttt.com/trigger/${IFTT_EVENT}/with/key/${IFTT_KEY}`,
+            uri: `https://maker.ifttt.com/trigger/${this.ifttEvent}/with/key/${this.ifttKey}`,
             method: 'POST',
             json: true,
             body: {
-                value1: status
+                value1: data
             }
         });
     }
-    sendLocationStatus (location) {
-        var ace = this;
-        return this.getTrainsTime(location).then((data) => {
-            return data.map((status) => {
-                return status.status;
-            });
-        }).then((data) => {
-            return ace.sendStatus(data.join(', '));
+    getStatus() {
+        return this.getTrains(true).then((trains) => {
+            return trains.map((train) => train.status).join('\n');
         });
     }
-
+    sendStatus() {
+        return this.getStatus().then((data) => {
+            if (data && data.length) {
+                return this._postIFTT(data);
+            } else {
+                return Promise.resolve(false);
+            }
+        });
+    }
 }
 
-let ace = new ACE();
-
-ace.sendLocationStatus(STOP_NAME).then((data) => {
-    console.log('Sent status for: ' + STOP_NAME);
+let ace = new ACE({
+    ifttKey: process.env.IFTT_KEY,
+    ifttEvent: process.env.IFTT_EVENT,
+    seed: {
+        get_vehicles: require('./seed_data/vehicles.json'),
+        get_stops: require('./seed_data/stops.json')
+    },
+    useSeed: false
 });
+if (ACTION === 'status') {
+    ace.getStatus().then((data) => {
+        if (data && data.length) {
+            console.log(data.join('\n'));
+        } else {
+            console.log('No trains running');
+        }
+    });
+} else {
+    ace.sendStatus()
+        .then((sent) => {
+            if (sent === false) {
+                console.log('No trains running. Status not sent.');
+            } else {
+                console.log('Status Sent Successfully');
+            }
+        })
+        .catch((err) => {
+            console.error(err.toString());
+        });
+}
